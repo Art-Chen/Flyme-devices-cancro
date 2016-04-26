0d4b7f8e107d8c345395f262819d5f4eb5378f08
Art-Chen <1263972314@qq.com>
2016-04-26 18:13:12 +0800

    Add some Information in Settings.(Untest)
    This Commit is created for Flyme5.Maybe cancro can't Upgrade Flyme 5 by me.but some thing is impossible.

--- custom_app.sh
+++ custom_app.sh
@@ -13,4 +13,11 @@ elif [ "$apkBaseName" = "TelephonyProvider" ];then
         echo ">>> in custom_app for $apkBaseName to adjust the interface "getAllMessagesFromIcc" with "getAllMessagesFromIccExtended""
         find $tempSmaliDir/ -name "*.smali" | xargs sed -i 's#invoke-static {}, Landroid\/telephony\/SmsManager;->getAllMessagesFromIcc()Ljava\/util\/ArrayList#invoke-static {}, Landroid\/telephony\/SmsManager;->getAllMessagesFromIccExtended()Ljava\/util\/ArrayList#g'
 
+# Untest Commit by Art_Chen to add something "zhuangbility".
+# Prepare to Flyme5.
+# Need build fullota to test.
+elif [ "$apkBaseName" = "Settings" ];then 
+ 		echo ">>> in custom_app for $apkBaseName to add my Information in Setting/AboutPhone" 
+ 	sed -i '3a\    <Preference android:title="ROM作者" android:summary="Art_Chen" style="?android:preferenceInformationStyle" />'  $tempSmaliDir/res/xml/device_info_settings.xml 
+
 fi
